160ebe24f101f9f34706b52b49316996c560fd39
Malenia <82274616+TheMalenia@users.noreply.github.com>
2024-05-18 19:34:25 +0330

    Update Human.java

--- exercise1/Human.java
+++ exercise1/Human.java
@@ -2,22 +2,22 @@ package exercise1;
 
 // 1.Change the staticPrint() method so that its output cannot be changed in all the classes that extend or implement Human.
 
-public abstract class Human {
+public abstract class Human{
     private String fullName;
 
-    public String getFullName() {
+    public String getFullName(){
         return fullName;
     }
 
-    public void setFullName(String fullName) {
+    public void setFullName(String fullName){
         this.fullName = fullName;
     }
 
-    public void sayMyName() {
+    public void sayMyName(){
         System.out.println("im a human!");
     }
 
-    public static final void staticPrint() {
+    public static final void staticPrint(){
         System.out.println("this function should always print this string in all subclasses");
     }
 }
