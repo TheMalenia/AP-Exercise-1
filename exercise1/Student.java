10e4caa52a11600adb410bd9b650281bd5b39353
Malenia <82274616+TheMalenia@users.noreply.github.com>
2024-05-18 19:23:02 +0330

    Update Student.java

--- exercise1/Student.java
+++ exercise1/Student.java
@@ -5,5 +5,41 @@ package exercise1;
 // 2. Change the student class so that when we call the sayMyName() method on an instance of this class, the fullName attribute of the student is printed.
 // 3.Add the following attributes to the student class with setters and getters: studentNumber, majorName, universityName
 
-public class Student {
+public class Student extends Human{
+    private String studentNumber;
+    private String majorName;
+    private String universityName;
+
+    public Student(String fullName){
+        super(fullName);
+    }
+    
+    // studentNumber
+    public String getStudentNumber(){
+        return studentNumber;
+    }
+    public void setStudentNumber(){
+        this.studentNumber = studentNumber;
+    }
+    
+    // majorName
+    public String getMajorName(){
+        return majorName;
+    }
+    public void setMajorName(String majorName){
+        this.majorName = majorName;
+    }
+    
+    // universityName
+    public String getUniversityName(){
+        return universityName;
+    }
+    public void setUniversityName(String universityName){
+        this.universityName = universityName;
+    }
+    
+    // sayMyName
+    public void sayMyName(){
+        System.out.println("Fullname:" + getFullName());
+    }
 }
